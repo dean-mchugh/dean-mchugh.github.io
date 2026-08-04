eb9f7a2fb35892554cab5ac47848b28fe304f39d
dean-mchugh <mchughde@tcd.ie>
2026-08-04 16:13:10 +0000

    Deploying to gh-pages from @ dean-mchugh/dean-mchugh.github.io@9895995cabf68bab0bc23cdad8af1f30cf144a45 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,7 +25,7 @@ ninja.data = [{
           },
         },{id: "nav-teaching",
           title: "teaching",
-          description: "Teaching and supervision experience.",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/teaching/";
